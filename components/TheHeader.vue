d119ba3c0d84d1ff1b38e4fc3f7b3be4b8dbe232
JeanDevFR <castellotti.j@gmail.com>
2023-01-20 13:11:10 +0100

    TheHeader component updated

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -7,21 +7,27 @@ function toggleColorMode() {
 </script>
 
 <template>
-  <header class="container mx-auto px-4 py-5 flex items-center justify-between">
+  <header
+    class="max-w-screen-lg mx-auto px-4 py-5 flex items-center justify-between"
+  >
     <div class="font-swanky text-slate-900 dark:text-white text-3xl">
       Jean Castellotti
     </div>
     <div class="flex gap-8">
       <div class="flex gap-3">
-        <div class="rounded-sm h-10 w-10 bg-sky-500 p-1 text-white flex">
+        <div
+          class="rounded-lg h-10 w-10 bg-sky-500 p-1 text-white flex transition hover:scale-105"
+        >
           <Icon name="bxl:linkedin" size="24px" class="m-auto" />
         </div>
-        <div class="rounded-sm h-10 w-10 bg-sky-500 p-1 text-white flex">
+        <div
+          class="rounded-lg h-10 w-10 bg-sky-500 p-1 text-white flex transition hover:scale-105"
+        >
           <Icon name="uil:github-alt" size="24px" class="m-auto" />
         </div>
       </div>
       <div
-        class="rounded-sm h-10 w-10 bg-slate-500 dark:bg-slate-400 p-1 text-white dark:text-slate-800 flex cursor-pointer"
+        class="rounded-lg h-10 w-10 bg-slate-500 dark:bg-slate-400 p-1 text-white dark:text-slate-800 flex cursor-pointer transition hover:scale-105"
         @click="toggleColorMode"
       >
         <ColorScheme placeholder="..." tag="span">
